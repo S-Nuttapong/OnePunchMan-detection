4d26d2822a75e9c2eb77fb103fa5bbc8c8ddf82f
S-Nuttapong <52415315+S-Nuttapong@users.noreply.github.com>
2019-07-04 01:07:46 +0700

    Utility script for resizing image
    resize image to square image while maintain original image's aspect ratio

---
+++ Resize_img.py
@@ -0,0 +1,29 @@
+import os
+from PIL import Image, ImageOps
+import glob
+
+'''
+pathIn = path to video
+pathOut = where output images will be saved
+name = name of file
+newsize = desired size
+'''
+
+if not os.path.exists(pathOut):
+    os.makedirs(pathOut)
+
+for i, file in enumerate(glob.glob(pathIn + '/*')):
+
+    fname = name + '_{str_0:0>{str_1}}.jpg'.format(str_0=i+1, str_1=int_desiredLength)
+    im = Image.open(file)
+    im.thumbnail((newsize), Image.ANTIALIAS)
+    apparent_size = im.size
+    apparent_h, apparent_w = im.size
+    desired_h, desired_w = newsize
+    delt_h, delt_w = abs(apparent_h - desired_h), abs(apparent_w - desired_w)
+
+    padding = (delt_h // 2, delt_w // 2, delt_h - (delt_h // 2), delt_w - (delt_w // 2))
+    im = ImageOps.expand(im, padding)
+    desired_size = im.size
+    print('{0}: {1}====>{2}'.format(fname, apparent_size, desired_size))
+    im.save(os.path.join(pathOut, fname))
